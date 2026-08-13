4b2a29640d0a375f9bfcf9f46199e3ea0890c568
Shreya-Borate <borateshreya43@gmail.com>
2026-08-13 12:05:53 +0530

    Dataset loaded

--- Play-Predictor-ML/Play_Predictor.py
+++ Play-Predictor-ML/Play_Predictor.py
@@ -3,3 +3,14 @@
 # PLAY PREDICTION BASED ON WEATHER CONDITIONS
 #
 ################################################################################################################
+
+#----------------- Import libraries -----------------
+import pandas as pd 
+
+
+
+#----------------- Load DataSet -----------------
+
+df = pd.read_csv("MarvellousInfosystems_PlayPredictor.csv")
+
+print("Dataset loaded Successfully.")
